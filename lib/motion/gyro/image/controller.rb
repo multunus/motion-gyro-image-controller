7dbfbb7cc815e7065551d92668ac0ee34301fe56
Jerry <jerry.j@multunus.com>
2014-03-06 18:39:27 +0530

    initial commit

---
+++ lib/motion/gyro/image/controller.rb
@@ -0,0 +1,11 @@
+require "motion/gyro/image/controller/version"
+
+module Motion
+  module Gyro
+    module Image
+      module Controller
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/motion/gyro/image/controller/version.rb
@@ -0,0 +1,9 @@
+module Motion
+  module Gyro
+    module Image
+      module Controller
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
